629b94ec3318b47bff037a8d3272c15416391689
ThucLinh312 <dothuclinh2006@gmail.com>
2025-07-14 23:21:09 +0700

    homework

---
+++ student/js/main.js
@@ -0,0 +1,11 @@
+let students = [];
+students.push("An", "Bình", "Chi");
+
+let index = students.indexOf("Bình");
+if (index !== -1) {
+  students.splice(index, 1);
+}
+h;
+students.unshift("Dũng");
+
+console.log("Danh sách học sinh :", students);
